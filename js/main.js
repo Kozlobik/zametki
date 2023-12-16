e5eb429f4cff6932ae919629bd07a12ff00e521a
Denis Kozlov <kozlobik007@mail.ru>
2023-12-16 18:28:42 +0100

    feat: we added movement to the menu!

--- js/main.js
+++ js/main.js
@@ -1,26 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
 
-    // Variables
-const colorModeBtn = document.querySelector('#color-mode');
-const colorModeMenu = document.querySelector('#color-mode-menu');
+    const colorModeBtn = document.querySelector('#color-mode');
+    const colorModeMenu = document.querySelector('#color-mode-menu');
+    const colorModeItem = document.querySelectorAll('.color-mode__choose-btn');
 
-    // Function
-const openColorModeMenu = () => {
-    colorModeMenu.classList.toggle('open');
-}
+    colorModeItem.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            colorModeMenu.classList.remove('open')
+        })
+    })
 
-    // Events
+    colorModeBtn.addEventListener('click', () => {
+        colorModeMenu.classList.toggle('open')
+    })
 
-colorModeBtn.addEventListener('click', openColorModeMenu());
-
-
-
-
-
-
-
-
-
-
-
-});
+}, false);
+    
