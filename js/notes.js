efff1a00c5a13707f75efd45d2f8c09544dc85d8
Denis Kozlov <kozlobik007@mail.ru>
2024-01-27 18:48:45 +0100

    feat: created a function of notes render!

--- js/notes.js
+++ js/notes.js
@@ -6,11 +6,13 @@
 
 const addNoteInput = document.querySelector('#inputAdd');
 const addNoteForm = document.querySelector('#addNoteForm');
+const notesList = document.querySelector('.notes__list');
 const allNotes = [];
 
 const pushNewNote = () => {
     let addNoteInputValue = addNoteInput.value.trim();
     if (addNoteInputValue.length > 0) {
+        renderNote(addNoteInputValue)
         allNotes.push(addNoteInputValue)
         addToLocal(allNotes);
     }
@@ -24,4 +26,23 @@ const addToLocal = (allNotes) => {
     localStorage.setItem('allNotes', JSON.stringify(allNotes));
 }
 
+const renderNote = (addNoteInputValue) => {
+    const note = 
+    `
+    <li class="note">
+    <div class="note__icon-star">
+       <svg class="star-svg">
+          <use xlink:href = "#star"></use>
+       </svg>
+    </div>
+    <textarea class="note__text">${addNoteInputValue}</textarea>
+    <button class="trash-btn">
+       <svg class="trash-svg">
+          <use xlink:href = "#trash"></use>
+       </svg>
+    </button>
+ </li>
+    `;
+    notesList.insertAdjacentHTML('afterbegin', note);
+}
 addNoteForm.addEventListener('submit', pushNewNote);
